7d2e042317e79f23aaf082253da32b0101d5e21e
Joon Kim <kimjoon2014@gmail.com>
2020-08-05 13:22:36 +0900

    tab config

---
+++ joon_kim_resume_dev/lib/blogTab.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class BlogTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('blogTab'),
+      ),
+    );
+  }
+}
---
+++ joon_kim_resume_dev/lib/rootPage.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:joon_kim_resume_dev/mainPage.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MainPage();
+  }
+}
